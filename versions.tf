1d3b504aae4fbbe986b0581ce436a80711ea00bf
Thierry Lei <thierry.lei-ext@fr.clara.net>
2022-03-28 19:22:06 +0200

    AZ-615: Add an option to enable or disable default tags

--- versions.tf
+++ versions.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 2.10"
+      version = ">= 2.10, < 3"
     }
     azurecaf = {
       source  = "aztfmod/azurecaf"
