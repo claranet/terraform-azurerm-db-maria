3794b062157710433e7687c717ad3c093aa6daec
Jérôme Respaut <jerome.respaut@fr.clara.net>
2022-06-09 15:41:43 +0200

    AZ-770: Put module version in a dedicated file

--- outputs-module.tf
+++ outputs-module.tf
@@ -2,7 +2,7 @@ output "terraform_module" {
   description = "Information about this Terraform module"
   value = {
     name       = "db-maria"
-    version    = "v5.0.0"
+    version    = file("${path.module}/VERSION")
     provider   = "azurerm"
     maintainer = "claranet"
   }
